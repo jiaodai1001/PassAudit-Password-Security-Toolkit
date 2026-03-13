91720d71216bf663bd34650c81952a1115dbfeab
jiaodai1001 <zixingxuanbu@gmail.com>
2026-03-13 17:35:55 +0800

    Delete passaudit directory

--- passaudit/tests/test_entropy.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from core.entropy import calculate_entropy
-
-print("password entropy:", calculate_entropy("password"))
-print("P@ssw0rd123 entropy:", calculate_entropy("P@ssw0rd123"))
